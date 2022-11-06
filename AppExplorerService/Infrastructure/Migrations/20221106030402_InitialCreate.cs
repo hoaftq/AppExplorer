193e39d79357965c24127330f9cb58ae6e9c8805
hoaftq <hoaftq@gmail.com>
2022-11-06 15:19:38 +0800

    Added migrations

---
+++ AppExplorerService/Infrastructure/Migrations/20221106030402_InitialCreate.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    internal partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Language",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Language", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "App",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppInfo_ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppInfo_Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppInfo_ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppUrls_SourceUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppUrls_ProductUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppUrls_LibUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppUrls_DownloadUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppUrls_ArticleUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_App", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_App_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppLanguage",
+                columns: table => new
+                {
+                    AppId = table.Column<long>(type: "bigint", nullable: false),
+                    LanguageId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppLanguage", x => new { x.AppId, x.LanguageId });
+                    table.ForeignKey(
+                        name: "FK_AppLanguage_App_AppId",
+                        column: x => x.AppId,
+                        principalTable: "App",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppLanguage_Language_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Language",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_App_CategoryId",
+                table: "App",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLanguage_LanguageId",
+                table: "AppLanguage",
+                column: "LanguageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppLanguage");
+
+            migrationBuilder.DropTable(
+                name: "App");
+
+            migrationBuilder.DropTable(
+                name: "Language");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
