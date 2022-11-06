7bc447efb44725f2710e4db32475dfec797a76ef
hoaftq <hoaftq@gmail.com>
2022-11-06 15:19:38 +0800

    Added migrations

---
+++ AppExplorerService/Infrastructure/Migrations/20221106030513_CustomMigration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    internal partial class CustomMigration : Infrastructure.CustomMigration.CustomMigration
+    {
+    }
+}
