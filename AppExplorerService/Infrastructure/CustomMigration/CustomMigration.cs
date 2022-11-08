00316f626fcaff88e08a25c888b88065e522a5bb
hoaftq <hoaftq@gmail.com>
2022-11-08 17:22:45 +0800

    Removed trigger & used changetracker events to update created data & updated date

--- AppExplorerService/Infrastructure/CustomMigration/CustomMigration.cs
+++ AppExplorerService/Infrastructure/CustomMigration/CustomMigration.cs
@@ -6,14 +6,12 @@ namespace Infrastructure.CustomMigration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            AppUpdateTrigger.Up(migrationBuilder);
             InitialData.Up(migrationBuilder);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             InitialData.Down(migrationBuilder);
-            AppUpdateTrigger.Down(migrationBuilder);
         }
     }
 }
